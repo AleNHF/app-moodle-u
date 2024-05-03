05f86e527a3f4088aa640564c514651af0d73991
Montaño Flores Diana Erika <dianaerika.montano16@gmail.com>
2024-05-03 10:40:30 -0400

    Removed unnecessary imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+
 import 'package:provider/provider.dart';
-import 'package:uagrm_app_moodle/courses/view/course_screen.dart';
+
 import 'package:uagrm_app_moodle/courses/viewmodel/course_viewmodel.dart';
 import 'package:uagrm_app_moodle/router/app_routes.dart';
 import 'package:uagrm_app_moodle/theme/app_theme.dart';
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
-// import 'package:uagrm_app_moodle/home/view/home_view.dart';
-import 'package:uagrm_app_moodle/utils/navbar/app_bar.dart';
 
 void main() async {
   // await dotenv.load(fileName: '.env');
